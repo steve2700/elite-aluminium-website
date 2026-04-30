fd7803459c89f46786ee84d1230e9ecce3028eaa
Stewart Nyaruwata <112034196+steve2700@users.noreply.github.com>
2026-04-30 13:41:02 +0200

    Add redirects for elitealuminium.co.za

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,15 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
- 
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'elitealuminium.co.za' }],
+        destination: 'https://www.elitealuminium.co.za/:path*',
+        permanent: true,
+      },
+    ]
+  },
 }
-
 export default nextConfig
